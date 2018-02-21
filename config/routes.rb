8b3792285c3dbed1836ab20ce4bdc8ac8c49fd77
JordynK91 <JordynK91@gmail.com>
2018-02-21 17:56:28 -0500

    end of the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "users#index"
+
+  get 'users/new'
+  post 'users', to: 'users#create'
+
+  post '/login', to: 'users#create_session'
+  
+  post '/logout', to: 'users#destroy_session'
+
+
 end
