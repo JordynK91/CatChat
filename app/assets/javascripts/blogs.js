dcdb467592a0045cb35f06c941c174327c2492b9
JordynK91 <JordynK91@gmail.com>
2018-02-28 16:43:47 -0500

    jquery

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -2,19 +2,26 @@
 
 
 
-window.onload = function(){
-	var editComment = document.getElementsByClassName('edit_comment')
-	var editor = document.getElementsByClassName("editor")
+// window.onload = function(){
+// 	var editComment = document.getElementsByClassName('edit_comment')
+// 	var editor = document.getElementsByClassName("editor")
 
-	for(i=0;i<editor.length;i++){
-		editor[i].addEventListener('click', function(event){
-			showForm(i)
-		})
-	}
+// 	for(var i=0;i<editor.length;i++){
+// 		editor[i].addEventListener('click', function(event){
+// 			showForm(i)
+// 		})
+// 	}
 
-	function showForm(x){
-		event.preventDefault()
-		editComment[x].style.display = "block";
-	}
+// 	function showForm(x){
+// 		event.preventDefault()
+// 		editComment[x].style.display = "block";
+// 	}
 
-}
+// }
+
+$(document).ready(function() {
+   $(".editor").click(function(event){
+   	event.preventDefault()
+     $(".editor").siblings(".edit_comment").toggle();
+ });
+});
