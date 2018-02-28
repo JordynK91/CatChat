a587960c97ebaaed70f77242418414b57b3b34bf
JordynK91 <JordynK91@gmail.com>
2018-02-28 17:24:16 -0500

    troubleshooting jquery

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -1,12 +1,12 @@
 // function used to toggle the edit options for a comment
 
 
-
+// below does not work when deployed to heroku because 'let' is not valid
 // window.onload = function(){
 // 	var editComment = document.getElementsByClassName('edit_comment')
 // 	var editor = document.getElementsByClassName("editor")
 
-// 	for(var i=0;i<editor.length;i++){
+// 	for(let i=0;i<editor.length;i++){
 // 		editor[i].addEventListener('click', function(event){
 // 			showForm(i)
 // 		})
@@ -19,8 +19,21 @@
 
 // }
 
-$(document).ready(function() {
-   $(".editor").click(function(event){
-     $(".editor").siblings(".edit_comment").toggle();
- });
-});
+// $(document).ready(function() {
+//    $(".editor").click(function(event){
+//    	event.preventDefault()
+//      $(".editor").siblings(".edit_comment").toggle();
+//  });
+// });
+
+$(document).ready(function(){
+ 	$(".editor").click(function(event){
+ 		event.stopPropagation()
+ 		if($(".edit_comment").css("display") == "none"){	
+ 			$(".editor").siblings(".edit_comment").css("display","block")}	
+ 		else {
+ 			$(".editor").siblings(".edit_comment").css("display","none")}
+ 	}
+
+ )})
+
